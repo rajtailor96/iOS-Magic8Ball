7cfcf622dbb8cbde83af1b2418fda790b13c11c6
rajtailor96 <rajtailor96@gmail.com>
2021-02-05 19:32:44 -0600

    Updated Randomization for Image UI using an Array of Image Literals

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        let randomGenerated = Int.random(in: 0..<5)
+        imageView.image = ballArray[randomGenerated]
+    }
+    
 }
 
